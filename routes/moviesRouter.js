ce3ecc4943df249d6cb343e2e2521753993829c5
Bryce Sexton <brycesexton15@gmail.com>
2024-01-23 16:41:38 -0800

    added routes

--- routes/moviesRouter.js
+++ routes/moviesRouter.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const movieCtrl = require('../controllers/moviesController')
 
-
-
+router.post('/', movieCtrl.create)
+router.get('/', movieCtrl.index)
+router.get('/:id', movieCtrl.show)
+router.get('/:movieId/performers/performerId', movieCtrl.addPerformer)
 
 
 
